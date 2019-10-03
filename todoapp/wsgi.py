75e509aca38d47c058ec98ba0afd35588f8a6191
hardikajmani <hardikajmani@gmail.com>
2019-10-03 20:19:26 +0530

    Hello

--- todoapp/wsgi.py
+++ todoapp/wsgi.py
@@ -11,7 +11,8 @@ import os
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellodjango.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellodjango.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todoapp.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
