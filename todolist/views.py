8d437da10a3d6d41700d9827d09a093fd38ff230
hardikajmani <hardikajmani@gmail.com>
2019-10-04 15:45:37 +0530

    Completed!

--- todolist/views.py
+++ todolist/views.py
@@ -10,13 +10,12 @@ from todolist.forms import SignUpForm, ExportForm
 
 @login_required(login_url='/accounts/login/')
 # Create your views here.
-
 def index(request): #the index view
     todos      = TodoList.objects.all()
     categories = Category.objects.all() #getting all categories with object manager
     priorities = Priority.objects.all() #getting all priorites with object manager
     status     = "Incomplete"
-    message = ""        
+    message    = ""
 
     if request.method == "POST": #checking if the request method is a POST
         if "taskAdd" in request.POST: #checking if there is a request to add a todo
@@ -27,35 +26,45 @@ def index(request): #the index view
             content = title + " -- " + date + " " + category + " priority@" + priority + " " + "Status: \"" #content
             Todo = TodoList(title=title, content=content, due_date=date, category=Category.objects.get(name=category), priority=Priority.objects.get(name = priority), status = status)
             Todo.save() #saving the todo 
-            return redirect("/",{"message":message}) #reloading the page
+            return redirect("/") #reloading the page
 
         if "taskDelete" in request.POST: #checking if there is a request to delete a todo
-            checkedlist = request.POST["checkedbox"] #checked todos to be deleted
-            if checkedlist == []:
-                message = "Please Choose atleast one task!"
-            else:    
-                for todo_id in checkedlist:
-                    todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
-                    todo.delete() #deleting todo
+            try:
+                checkedlist = request.POST["checkedbox"] #checked todos to be deleted
+                if type(checkedlist) is str:
+                    todo = TodoList.objects.get(id=int(checkedlist)) #getting todo id
+                    todo.delete() 
+                else:
+                    for todo_id in checkedlist:
+                        print(type(todo_id), todo_id)
+                        todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
+                        todo.delete() #deleting todo
                 message  = "Updated!"
-            return redirect("/",{"message":message})
+                
+            except:
+                message = "Please choose atleast one task!"                
 
         if "taskComplete" in request.POST: #checking if there is a request to delete a todo
-            checkedlist = request.POST["checkedbox"] #checked todos to be deleted
-            if checkedlist == []:
-                message = "Please Choose atleast one task!"
-            else:
-                for todo_id in checkedlist:
-                    todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
+            try:
+                checkedlist = request.POST["checkedbox"] #checked todos to be deleted
+                if type(checkedlist) is str:
+                    todo = TodoList.objects.get(id=int(checkedlist)) #getting todo id
                     todo.status = "Complete" #finshing todo
                     todo.save()
+                else:
+                    for todo_id in checkedlist:
+                        todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
+                        todo.status = "Complete" #finshing todo
+                        todo.save()
                 message  = "Updated!"
-            return redirect("/",{"message":message})
+            except:
+                message = "Please choose atleast one task!"
+
             
 
         if "deleteAll" in request.POST: #checking if there is a request to delete a todo
-            for todo in todos:
-                todo.delete()
+            TodoList.objects.all().delete()
+            message  = "Updated!"
 
     return render(request, "index.html", {"todos": todos, "categories":categories, "priorities":priorities, "status":status, "message":message})
 
@@ -75,6 +84,7 @@ def signup(request):
     return render(request, 'signup.html', {'form': form})
 
 def export(request):
+
     if request.method == 'POST':
         form = ExportForm(request.POST)
         if form.is_valid():
